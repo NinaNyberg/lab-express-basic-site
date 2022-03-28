45be6dab6b8a7accc12ce2b4306fb26fe99bc810
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-03-29 00:58:27 +0300

    Second commit: dynamics added

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 
-app.get('/home', (request, response) => {
-  response.sendFile(__dirname + '/views/home.html');
+app.get('/', (request, response) => {
+  response.render('home');
 });
 
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about');
 });
 
 app.get('/works', (request, response) => {
-  response.sendFile(__dirname + '/views/works.html');
+  const works = [
+    { image: '/images/bookone.jpg', name: 'Himoshoppaajan joulu', year: 2000 },
+
+    { image: '/images/booktwo.jpg', name: 'Tavataanko oikeasti?', year: 2001 },
+
+    { image: '/images/bookthree.jpg', name: 'Kadonnut: Audrey', year: 2002 }
+  ];
+
+  response.render('works', { works });
 });
 
 app.listen(3000);
